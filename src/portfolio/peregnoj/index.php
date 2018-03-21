e4c07dade3691c0868ae4c165340511cc5e393d2
Sidderman <s.i.sidorovkrd@yandex.ru>
2018-03-21 13:27:26 +0300

    Добавил кейс по перегною

---
+++ src/portfolio/peregnoj/index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,700&amp;subset=cyrillic" rel="stylesheet">
+	<link rel="stylesheet" href="/css/style.css?v=1480333475">
+	<link rel="icon" type="image/PNG" href="/favicon.png">
+	<meta name="description" content="Народное хозяйство - продажа перегноя в Интернете" />
+	<title>Народное хозяйство - продажа перегноя в Интернете</title>
+</head>
+
+
+<body>
+
+	<div class="main-inner main-portfolio">
+
+		<?php include ($_SERVER['DOCUMENT_ROOT'] . '/modules/header.php'); ?>
+
+		<section class="case">
+			<div class="container">
+				<span class="case__border"></span>
+				<div class="case__block">
+					<div class="case__block-head">
+						<span class="case__strip"></span>
+						<h1 class="case__title case__title--main">Народное хозяйство - продажа перегноя в Интернете</h1>
+						<p class="case__text case__text--date">Дата выпуска: 20.02.2018</p>
+					</div>
+					<div class="case__frame-wrapper">
+						<div class="case__frame">
+							<img src="/img/peregnoj_site.png" alt="" class="case__frame-img">
+						</div>
+					</div>
+				</div>
+				<div class="case__block">
+					<div class="case__block-head">
+						<span class="case__strip"></span>
+						<h2 class="case__title">ЗАДАЧА</h2>
+						<p class="case__text">Найти новый канал продаж продукции производителя «Народное хозяйство». А именно продавать перегной в интернете. Перегной - составная часть почвы из перегнивших растительных и животных остатков. Если коротко – удобрение для посадки всяких помидор и кабачков.
+
+							Целевая аудитория – садоводы с загорелыми спинами в панамах, на которых скорее можно воздействовать через спутниковое ТВ, чем через интернет.
+
+							Но точку воздействия мы нашли – это дети садоводов. Дяденьки и тетеньки, которые приезжают на сады-фазенды с одной целью – пожарить шашлы. Им не особо хочется вникать в процессы выращивания гигантских китайский огурцов, но выполняют все просьбы своих родителей.
+
+							Прояснился один момент – целевая аудитория, на которую следует воздействовать в интернете – это дети садоводов. Просьбу родителей они выполняют четко.
+
+							Итак, наша цель – запросы на доставку и разгрузку перегноя на участок мамы без лишних телодвижений и заморочек.
+
+						Основной элемент воздействия – аудио-отзывы реальных садоводов, мам и пап нашей аудитории. Т.к. уровень доверия к людям, похожим на родителей стремится к бесконечности.</p>
+					</div>
+					<a href="/web_service/" class="main__btn case__btn">Заказать сайт</a>
+				</div>
+				<div class="case__block">
+					<div class="case__block-head">
+						<span class="case__strip"></span>
+						<h2 class="case__title">УСЛУГИ</h2>
+						<ul class="case__list">
+							<li class="case__item"><a href="#">Разработка сайта</a></li>
+							<li class="case__item"><a href="#">Дизайн сайта</a></li>
+							<li class="case__item"><a href="#">Контекстная реклама</a></li>
+							<li class="case__item"><a href="#">SEO</a></li>
+							<li class="case__item"><a href="#">Упаковка</a></li>
+						</ul>
+						<img src="/img/peregnoj_img.png" alt="" class="case__img case__img--small">
+					</div>
+				</div>
+				<div class="case__block">
+					<div class="case__block-head">
+						<span class="case__strip"></span>						
+						<h2 class="case__title">АКЦЕНТЫ</h2>
+						<ul class="case__list">
+							<li class="case__item">Простота сайта</li>
+							<li class="case__item">Аудио отзывы бабушек и дедушек</li>
+							<li class="case__item">Прямое предложение</li>
+						</ul>
+					</div>
+				</div>
+				<div class="case__block">
+					<div class="case__block-head">
+						<span class="case__strip"></span>
+						<h2 class="case__title">РЕЗУЛЬТАТ</h2>
+						<p class="case__text">Навоз отлично продается в интернете и инстаграме. Конверсия в заказ с Я.Директа в сезонные периоды достигает 63%. Средняя конверсия 21%. В инстаграме отлично работают посты на хайповые темы. Итого: все работает хорошо, заказов много, бюджеты скромные.</p>
+					</div>
+					<a href="/web_service/" class="main__btn case__btn">Как это работает?</a>
+				</div>
+				<div class="case__block">
+					<div class="case__block-head">
+						<span class="case__strip"></span>
+						<h2 class="case__title">ДЕЯТЕЛИ</h2>
+						<ul class="case__list">
+							<li class="case__item"><span>STO:</span> <a href="#">Артем</a></li>
+							<li class="case__item"><span>Designer:</span> <a href="#">Алексей</a></li>
+							<li class="case__item"><span>Front-End dev.:</span> <a href="#">Сергей</a></li>
+							<li class="case__item"><span>Back-End dev.:</span> <a href="#">Сергей, Эрик</a></li>
+							<li class="case__item"><span>SEO/SMM:</span> <a href="#">Даниил</a></li>
+						</ul>
+						
+					</div>
+				</div>
+				<div class="case__block case__block--tag">
+					<div class="portfolio__tag-wrapper">
+						<a href="#" class="portfolio__tag">Как это работает?</a>
+					</div>
+				</div>
+			</div>
+		</section>
+		<?php include ($_SERVER['DOCUMENT_ROOT'] . '/modules/footer.php'); ?>
+
+	</body>
+	</html>
